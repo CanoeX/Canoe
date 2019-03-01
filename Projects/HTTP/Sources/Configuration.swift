796b343d086e62babdc056ab10f9a260d6c25591
Maxim Berezhnoy <berezhnoy.maxim@gmail.com>
2019-03-01 01:19:29 +0100

    minor fixes for default headers

--- Projects/HTTP/Sources/Configuration.swift
+++ Projects/HTTP/Sources/Configuration.swift
@@ -4,38 +4,38 @@
 //
 
 import Foundation
-import Utilities
 import UIKit
+import Utilities
 
 public struct Configuration {
-    // TODO: Reddit API client that will use this HTTP module has to additionally specify reddit username of the app developer
+    // TODO: Reddit API client that will use this HTTP module has to additionally specify the reddit username of the app developer
     public static let defaultHeaders: Headers = {
         let acceptEncoding = "gzip;q=1.0, compress;q=0.5"
-        
+
         let acceptLanguage = Locale.preferredLanguages.prefix(10).enumerated().map { i, langCode in
             let quality = 1.0 - Double(i) * 0.1
-            "\(langCode);q=\(quality)"
+            return "\(langCode);q=\(quality)"
         }.joined(separator: ", ")
-        
-        let userAgent = {
-            guard let bundleInfo = Bundle.main.infoDictionary else { 
+
+        let userAgent: String = {
+            guard let bundleInfo = Bundle.main.infoDictionary else {
                 return "Canoe"
             }
-            
+
             let appVersion = bundleInfo["CFBundleShortVersionString"] as? String ?? "Unknown app version"
             let bundle = bundleInfo[kCFBundleIdentifierKey as String] as? String ?? "Unknown"
             let deviceName = UIDevice.current.model
             let osName = UIDevice.current.systemName
             let osVersion = UIDevice.current.systemVersion
-            
+
             // Example: iOS (9.0, iPhone):com.canoex.canoe:1.0
-            "\(osName) (\(osVersion), \(deviceName)):\(bundle):\(appVersion)"
+            return "\(osName) (\(osVersion), \(deviceName)):\(bundle):\(appVersion)"
         }()
-                
-        [
-            "Accept-Encoding": acceptEnconding,
+
+        return [
+            "Accept-Encoding": acceptEncoding,
             "Accept-Language": acceptLanguage,
-            "User-Agent": userAgent 
+            "User-Agent": userAgent
         ]
     }()
 
@@ -43,11 +43,11 @@ public struct Configuration {
             requestTimeout: 1.minutes,
             additionalHeaders: defaultHeaders
     )
-    
+
     public let requestTimeout: TimeInterval
     private let additionalHeaders: Headers
-    
+
     public var headers: Headers {
-        return Configuration.defaultHeaders.merging(additionalHeaders, uniquingKeysWith: {_, r in r})
+        return Configuration.defaultHeaders.merging(additionalHeaders, uniquingKeysWith: { _, r in r })
     }
 }
